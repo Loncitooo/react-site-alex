c37836840e5f157242e704a2fe89b2ece44a336b
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-02 18:13:20 -0700

    Adjust padding for brief section title for improved alignment

--- src/views/home/brief-section.jsx
+++ src/views/home/brief-section.jsx
@@ -15,7 +15,7 @@ export const Brief = () => {
   return (
     <div className="relative w-full z-10 flex flex-col items-end gap-4 p-4 md:p-6 ">
       <div className=" absolute -left-10 w-max h-auto bg-orange-500 px-10 py-2 flex justify-end md:w-1/2 md:justify-start ">
-      <h1 className=" text-center uppercase font-bold text-white text-3xl tracking-widest "> {t("brief")}</h1>
+      <h1 className=" pl-10 text-center uppercase font-bold text-white text-3xl tracking-widest "> {t("brief")}</h1>
       </div>
       {/* <div className="w-full h-[2px] bg-orange-400 mb-7"></div> */}
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 mt-20">
