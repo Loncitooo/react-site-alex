be49a7d09cff11eaada5c6a0e15a7ac692e299f9
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-02 17:46:31 -0700

    Add internationalization support for Brief section and BriefCard component

--- src/views/home/brief-section.jsx
+++ src/views/home/brief-section.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import BriefCard from "../../components/brief/brief-card";
 import {
   FaCar,
@@ -10,42 +11,43 @@ import {
 } from "react-icons/fa";
 
 export const Brief = () => {
+  const { t } = useTranslation();
   return (
     <div className="relative w-full z-10 flex flex-col items-end gap-4 ">
       <div className=" absolute -left-10 w-max h-auto bg-orange-500 px-10 py-2 flex justify-end md:w-1/2 md:justify-start ">
-      <h1 className=" text-center uppercase font-bold text-white text-3xl tracking-widest ">Brief</h1>
+      <h1 className=" text-center uppercase font-bold text-white text-3xl tracking-widest "> {t("brief")}</h1>
       </div>
       {/* <div className="w-full h-[2px] bg-orange-400 mb-7"></div> */}
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 mt-20">
         <BriefCard
           icon={<FaCar />}
-          title="Expert in the Mexican Auto Industry"
-          text="Top expert on the Mexican Auto Industry, its management and labor transitions"
+          title={t("briefCard.briefTitle1")}
+          text={t("briefCard.briefDescription1")}
         />
         <BriefCard
           icon={<FaHandshake />}
-          title="Specialist in Labor Relations and Just Transitions"
-          text="Top expert on industrial and labor relations systems changes, labor relations and just transition "
+          title={t("briefCard.briefTitle2")}
+          text={t("briefCard.briefDescription2")}
         />
         <BriefCard
           icon={<FaCogs />}
-          title="Specialist in Socio-Technical and Energy Transitions"
-          text="Socio-technical, energy and industrial transition specialist "
+          title={t("briefCard.briefTitle3")}
+          text={t("briefCard.briefDescription3")}
         />
         <BriefCard
           icon={<FaChargingStation />}
-          title="Specialist in Transport Electrification and Sustainable Mobility"
-          text="Transport electrification and sustainable mobilities specialist "
+          title={t("briefCard.briefTitle4")}
+          text={t("briefCard.briefDescription4")}
         />
         <BriefCard
           icon={<FaTrophy />}
-          title="National Researcher Level III (Highest Rank)"
-          text="Highest rank as Mexican National Research –Level III "
+          title={t("briefCard.briefTitle5")}
+          text={t("briefCard.briefDescription5")}
         />
         <BriefCard
           icon={<FaNetworkWired />}
-          title="Leader in National and International Research Networks"
-          text="Leadership in creating and promoting national and international research networks"
+          title={t("briefCard.briefTitle6")}
+          text={t("briefCard.briefDescription6")}
         />
       </div>
     </div>
